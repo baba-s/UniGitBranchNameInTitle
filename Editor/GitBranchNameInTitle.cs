73c35d69cfedef56c6e5155155e20266ad1dcce4
baba_s <shotababa1206@gmail.com>
2020-06-06 21:43:11 +0900

    fix

--- Editor/GitBranchNameInTitle.cs
+++ Editor/GitBranchNameInTitle.cs
@@ -1,6 +1,7 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Linq;
 using System.Reflection;
-using UniEditorWindowTitleChanger;
 using UniGitUtils;
 using UnityEditor;
 using UnityEditorInternal;
@@ -16,6 +17,28 @@ namespace Kogane.Internal
 	internal static class GitBranchNameInTitle
 	{
 		//==============================================================================
+		// 定数
+		//==============================================================================
+		private const BindingFlags UPDATE_MAIN_WINDOW_TITLE_ATTR          = BindingFlags.Static | BindingFlags.NonPublic;
+		private const string       APPLICATION_TITLE_DESCRIPTOR_FULL_NAME = "UnityEditor.ApplicationTitleDescriptor";
+
+		//==============================================================================
+		// 定数(static readonly)
+		//==============================================================================
+		private static readonly Type     EDITOR_APPLICATION_TYPE = typeof( EditorApplication );
+		private static readonly Assembly EDITOR_ASSEMBLY         = EDITOR_APPLICATION_TYPE.Assembly;
+		private static readonly Type[]   EDITOR_TYPES            = EDITOR_ASSEMBLY.GetTypes();
+
+		private static readonly Type APPLICATION_TITLE_DESCRIPTOR_TYPE = EDITOR_TYPES
+			.FirstOrDefault( c => c.FullName == APPLICATION_TITLE_DESCRIPTOR_FULL_NAME );
+
+		private static readonly EventInfo UPDATE_MAIN_WINDOW_TITLE_EVENT_INFO =
+			EDITOR_APPLICATION_TYPE.GetEvent( "updateMainWindowTitle", UPDATE_MAIN_WINDOW_TITLE_ATTR );
+
+		private static readonly MethodInfo UPDATE_MAIN_WINDOW_TITLE_METHOD_INFO =
+			EDITOR_APPLICATION_TYPE.GetMethod( "UpdateMainWindowTitle", UPDATE_MAIN_WINDOW_TITLE_ATTR );
+
+		//==============================================================================
 		// 関数(static)
 		//==============================================================================
 		/// <summary>
@@ -23,14 +46,32 @@ namespace Kogane.Internal
 		/// </summary>
 		static GitBranchNameInTitle()
 		{
-			UpdateDisp();
-			EditorApplication.playModeStateChanged += state => UpdateDisp();
+			var delegateType = typeof( Action<> ).MakeGenericType( APPLICATION_TITLE_DESCRIPTOR_TYPE );
+			var methodInfo   = ( ( Action<object> ) UpdateMainWindowTitle ).Method;
+			var del          = Delegate.CreateDelegate( delegateType, null, methodInfo );
+			var nonPublic    = true;
+			var parameters   = new object[] { del };
+
+			UPDATE_MAIN_WINDOW_TITLE_EVENT_INFO.GetAddMethod( nonPublic ).Invoke( null, parameters );
 		}
 
 		/// <summary>
 		/// タイトルの表示を更新します
 		/// </summary>
-		private static void UpdateDisp()
+		/// <param name="desc"></param>
+		private static void UpdateMainWindowTitle( object desc )
+		{
+			var fieldInfo = APPLICATION_TITLE_DESCRIPTOR_TYPE
+				.GetField( "title", BindingFlags.Instance | BindingFlags.Public );
+
+			var title = GetTitle();
+			fieldInfo.SetValue( desc, title );
+		}
+
+		/// <summary>
+		/// タイトルに表示する文字列を返します
+		/// </summary>
+		private static string GetTitle()
 		{
 			// https://github.com/Unity-Technologies/UnityCsReference/blob/master/Editor/Mono/EditorApplication.cs
 			var activeSceneName = L10n.Tr( "Untitled" );
@@ -53,7 +94,7 @@ namespace Kogane.Internal
 				title += $" - {branchName}";
 			}
 
-			EditorWindowTitleChanger.SetTitle( title );
+			return title;
 		}
 
 		/// <summary>
